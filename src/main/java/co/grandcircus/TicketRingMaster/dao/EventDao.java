4cb315677024787209240a06a078c005c010c82f
grahamcwycyshyn <grahamcwycyshyn@gmail.com>
2019-08-08 11:41:43 -0400

    fourth commit

--- src/main/java/co/grandcircus/TicketRingMaster/dao/EventDao.java
+++ src/main/java/co/grandcircus/TicketRingMaster/dao/EventDao.java
@@ -1,5 +1,15 @@
 package co.grandcircus.TicketRingMaster.dao;
 
-public class EventDao {
+import javax.transaction.Transactional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import co.grandcircus.TicketRingMaster.entity.Event;
+
+@Repository
+@Transactional
+public interface EventDao extends JpaRepository<Event, Long> {
+
+	
 }
--- src/main/java/co/grandcircus/TicketRingMaster/entity/Event.java
+++ src/main/java/co/grandcircus/TicketRingMaster/entity/Event.java
@@ -1,5 +1,49 @@
 package co.grandcircus.TicketRingMaster.entity;
 
-public class Event {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
+public class Event {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private String locate;
+	private String dates;
+	private Boolean favorite;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getLocate() {
+		return locate;
+	}
+	public void setLocate(String locate) {
+		this.locate = locate;
+	}
+	public String getDates() {
+		return dates;
+	}
+	public void setDates(String dates) {
+		this.dates = dates;
+	}
+	public Boolean getFavorite() {
+		return favorite;
+	}
+	public void setFavorite(Boolean favorite) {
+		this.favorite = favorite;
+	}
 }
