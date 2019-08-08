2acc606565676ecdb02f84d3a1ad5e2a582f981e
grahamcwycyshyn <grahamcwycyshyn@gmail.com>
2019-08-08 11:12:04 -0400

    initial commit

---
+++ src/main/java/co/grandcircus/TicketRingMaster/TicketRingMasterApplication.java
@@ -0,0 +1,13 @@
+package co.grandcircus.TicketRingMaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketRingMasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketRingMasterApplication.class, args);
+	}
+
+}
---
+++ src/main/java/co/grandcircus/TicketRingMaster/TicketRingMasterController.java
@@ -0,0 +1,5 @@
+package co.grandcircus.TicketRingMaster;
+
+public class TicketRingMasterController {
+
+}
